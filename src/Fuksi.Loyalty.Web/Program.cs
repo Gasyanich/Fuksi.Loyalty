c62556d070c9414d430d894aca3fab4b2d69f954
Igor Igumenov <igumenovia@moffice.akbars.ru>
2023-09-15 15:19:31 +0400

    docker

--- src/Fuksi.Loyalty.Web/Program.cs
+++ src/Fuksi.Loyalty.Web/Program.cs
@@ -2,12 +2,11 @@ using Fuksi.Loyalty.Module.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 var services = builder.Services;
 services.AddControllers();
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+services.AddHttpClient();
 
 services.AddCors(options =>
     options.AddDefaultPolicy(policyBuilder => policyBuilder
@@ -20,16 +19,10 @@ services.AddCors(options =>
 
 services.AddAuthModule(builder.Configuration);
 
-services.AddHttpClient();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors();
 
@@ -38,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-await app.UseMigrations();
+//await app.UseMigrations();
 
 app.Run();
